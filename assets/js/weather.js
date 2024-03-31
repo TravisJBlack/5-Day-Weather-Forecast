76ca0c3867a7260180ab463adbbbf0471277677d
TravisJBlack <159502505+TravisJBlack@users.noreply.github.com>
2024-03-30 23:18:04 -0500

    updated javasciprt with functions to display data and added commets on javascript file

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,31 +1,46 @@
+// getting references to import from DOM elements.
 const searchedEl = document.getElementById('searched-cities');
 const cityInputEL = document.getElementById('city');
 const searchButton = document.getElementById('save');
 const forcast = document.getElementById('forcast');
-const searchedLocations = JSON.parse(localStorage.getItem('locations')) || [];
-function init() {
-    
-    console.log(searchedLocations);
-     if(searchedLocations.length === 0){
-         return;
-    }
+const dailyForcast = document.getElementById('daily-forecast');
+const historyButton = document.getElementById('#city');
 
+// declare gloabl variable to hold previous searches 
+const searchedLocations = JSON.parse(localStorage.getItem('locations')) || [];
+
+//function to run at page load to render list of previous searches.
+function init() {
+    // creates previous searched city into button on page 
     for(let i = 0; i < searchedLocations.length; i++){
       const cityContainer = document.createElement('button');
-      cityContainer.classList = 'w-100 previous-search';
+      cityContainer.classList = `w-100 btn btn-info bg-secondary border-0 my-1 data-${searchedLocations[i]}`;
+      cityContainer.setAttribute('id','city');
       cityContainer.textContent = `${searchedLocations[i]}`;
-      searchedEl.append(cityContainer);
+      searchedEl.prepend(cityContainer);
     }
-}
+};
 
 init();
 
+//handles getting data from the search button and then makes a history button out of it and transitions to getLatLon function while passing city name
 const citySubmit = function (event){
     event.preventDefault();
 
-    const enterCity = cityInputEL.value.trim();
+    const cityData = cityInputEL.value.trim();
+    const enterCity = cityData.charAt(0).toUpperCase()+cityData.slice(1);
     console.log(enterCity);
+
+    
+    
     if(enterCity) {
+    const cityContainer = document.createElement('button');
+    cityContainer.classList = `w-100 btn btn-info bg-secondary border-0 data-${enterCity}`;
+    cityContainer.setAttribute('id','city');
+    cityContainer.textContent = `${enterCity}`;
+    searchedEl.prepend(cityContainer);
+        searchedLocations.push(enterCity);
+        localStorage.setItem('locations',JSON.stringify(searchedLocations));
         getLatLon(enterCity);
     } else {
         alert('Please enter a city');
@@ -33,11 +48,12 @@ const citySubmit = function (event){
 
 };
 
-
-
-//getting data from api server
+//gets lattitude and longitude coordinates from sever
 const getLatLon = function (enterCity){
-    console.log(enterCity);
+    if(!enterCity){
+        enterCity = searchedLocations[searchedLocations.length-1];
+    }
+
     const apiKey = '3b52e6db8fc05a700ab6ed26ec8829e0';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${enterCity}&APPID=${apiKey}`;
 
@@ -52,6 +68,7 @@ const getLatLon = function (enterCity){
     });
 };
 
+//use the data we got from server to get weather data about searched city
 const get5DayForcast = function (lat, lon){
     const apiKey = '3b52e6db8fc05a700ab6ed26ec8829e0';
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&esclude=hourly,alerts&APPID=${apiKey}&units=imperial`;
@@ -60,8 +77,9 @@ const get5DayForcast = function (lat, lon){
         .then(function(response) {
         if (response.ok){
             response.json().then(function(data){
-                searchedLocations.push(data.city.name);
-                localStorage.setItem('locations',JSON.stringify(searchedLocations));
+                
+                console.log(data);
+        
                 displayForcast(data.city.name, data.list)
             });
         }   else {
@@ -70,17 +88,17 @@ const get5DayForcast = function (lat, lon){
     });
 };
 
+//displays wanted data from the server. Function creates dynamic elments with styling and classes and appends them to the html document
 const displayForcast = function (city, weatherList){
+    while (forcast.hasChildNodes()){
+        forcast.removeChild(forcast.firstChild);
+    }
+
     let dateFormat = dayjs(weatherList[0].dt_txt).format('(M/D/YYYY)');
     const weatherContainer = document.createElement('section');
     weatherContainer.classList = 'border border-dark p-3 mr-3';
     forcast.appendChild(weatherContainer);
 
-    const cityContainer = document.createElement('button');
-    cityContainer.classList = 'w-100';
-    cityContainer.textContent = `${city}`;
-    searchedEl.append(cityContainer);
-    
     const title = document.createElement('h4');
     title.textContent = `${city} ${dateFormat}`;
     weatherContainer.append(title);
@@ -96,13 +114,41 @@ const displayForcast = function (city, weatherList){
     const humidity = document.createElement('p');
     humidity.textContent = `Humidity: ${weatherList[0].main.humidity} %`;
     weatherContainer.append(humidity);
+    const array = [];
+    for (let i = 0; i < weatherList.length; i +=8){
+        
+        array.push(dayjs(weatherList[i].dt_txt).format('M/D/YYYY'), weatherList[i].main.temp, weatherList[i].wind.speed, weatherList[i].main.humidity);
+    }
+    while (dailyForcast.hasChildNodes()){
+        dailyForcast.removeChild(dailyForcast.firstChild);
+    }
+    console.log(array);
     
-    // const realList = JSON.parse(weatherList);
-    // console.log(realList);
-    // for (let i =8; i < realList.length; i += 8) {
-    //     console.log(`${realList[i]}`);
-    // }
+    for (let i = 0; i < array.length; i += 4){
+        const dailyCity = document.createElement('section');
+        
+        dailyCity.classList = 'd-block-flex bg-success text-light p-0 m-1 col-2 w-100';
+
+        dailyCity.innerHTML = `<p>${array[i]}<p>
+                                <p>Temp: ${array[i+1]}°F<p>
+                                <p>Wind: ${array[i+2]} MPH<p>
+                                <p>Humidity: ${array[i+3]}%<p>`
+        dailyForcast.appendChild(dailyCity);
+    }
     
 };
 
+
+//if no cities are in storage does nothing but if previous cities are avaialbe then it will run through functions to display the last one searched
+if(searchedLocations.length === 0){
+console.log('no city');
+} else {
+    getLatLon();
+}
+
+//waits for a click on the search button to start the fucntions 
 searchButton.addEventListener('click', citySubmit);
+
+// historyButton.addEventListener('click', function(){
+//     console.log('hello');
+// })
